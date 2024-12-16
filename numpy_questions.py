169430ba7991d61f498fdcc6a06eecf723336022
Denk-Em <hugo.mai@ensta-paris.fr>
2024-12-16 14:23:35 +0100

    Exo numpy & Wallis

--- numpy_questions.py
+++ numpy_questions.py
@@ -41,9 +41,23 @@ def max_index(X):
     j = 0
 
     # TODO
+    ### Error cases
+    if not isinstance(X, np.ndarray):
+        raise ValueError("The input is not a numpy array")
+    
+    if X.ndim != 2:
+        raise ValueError('The shape is not 2D')
+    
+    ### Function
+    max_element = X[0,0]
+    for k in range(X.shape[0]):
+        for l in range(X.shape[1]):
+            if X[k,l] > max_element:
+                max_element = X[k,l]
+                i, j = k, l
+    
 
     return i, j
-
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -64,4 +78,14 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    
+    product = 1
+    
+    if n_terms == 0:
+        return product 
+    
+    for i in range(1, n_terms):
+        product = product * (4*i**2) / (4*i**2 - 1)
+        
+    return 2 * product
+
