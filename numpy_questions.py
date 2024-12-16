924a97f4f48b8109c356e60d6acf5fc7fb0b580a
Zakarya elmimouni <zakartazaki2002@gmail.com>
2024-12-16 11:04:31 +0100

    flake8 checked

--- numpy_questions.py
+++ numpy_questions.py
@@ -41,9 +41,8 @@ def max_index(X):
         raise ValueError("Input must be a numpy array.")
     if X.ndim != 2:
         raise ValueError("Input array must be 2D.")
-
-    flat_index = np.argmax(X)  
-    i, j = divmod(flat_index, X.shape[1])  
+    flat_index = np.argmax(X)
+    i, j = divmod(flat_index, X.shape[1])
     return i, j
 
 
@@ -67,13 +66,11 @@ def wallis_product(n_terms):
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
     if n_terms == 0:
-        return 2.0  
+        return 2.0
 
     product = 1.0
     for i in range(1, n_terms + 1):
         numerator = 4 * i ** 2
         denominator = numerator - 1
         product *= numerator / denominator
-    
     return product * 2
- 
