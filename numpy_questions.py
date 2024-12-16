bbd920fdbb140508a560426f34cbdbb77b3fc196
BenjaminDschps <b.deschamps0723@outlook.fr>
2024-12-16 16:23:49 +0100

    correct pi value

--- numpy_questions.py
+++ numpy_questions.py
@@ -72,7 +72,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    pi = 1
+    pi = 2.0
     for i in range(1, n_terms+1):
         pi*=(2*i/(2*i-1))*(2*i/(2*i+1))
     return pi
