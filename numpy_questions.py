df92d0531f99af30002f4ce0ca6af723022b8e30
SAN <sardarahmednazar@gmail.com>
2024-12-16 14:27:44 +0100

    done

--- numpy_questions.py
+++ numpy_questions.py
@@ -17,10 +17,6 @@ errors by calling `flake8` at the root of the repo.
 """
 import numpy as np
 
-
-
-
-
 def max_index(X):
     """Return the index of the maximum in a numpy array.
 
@@ -32,7 +28,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
@@ -40,13 +36,13 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
-
-    # TODO
-
-    return i, j
-
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input must be a numpy array")
+    if X.ndim != 2:
+        raise ValueError("Input array must be 2D")
+    
+    index = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    return index
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -65,6 +61,11 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms < 0:
+        raise ValueError("Number of terms must be non-negative")
+    
+    product = 1.0
+    for n in range(1, n_terms + 1):
+        product *= (4 * n ** 2) / (4 * n ** 2 - 1)
+    
+    return product * 2
