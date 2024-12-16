515ab0ca0c27e74fc8c5a3b61bf97d78346c4728
TitouanCh <titouan85000@gmail.com>
2024-12-16 13:57:05 +0100

    formatting

--- numpy_questions.py
+++ numpy_questions.py
@@ -40,12 +40,15 @@ def max_index(X):
     i = 0
     j = 0
 
-    if not(isinstance(X, np.ndarray)): raise(ValueError)
-    if X.ndim != 2: raise(ValueError)
+    if not isinstance(X, np.ndarray):
+        raise ValueError
+    if X.ndim != 2:
+        raise ValueError
 
     m = None
     for (idx, a) in enumerate(X.ravel()):
-        if m is None: m = a
+        if m is None:
+            m = a
         elif a > m:
             m = a
             j = idx % X.shape[1]
@@ -73,7 +76,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    if n_terms < 0: raise(ValueError)
+    if n_terms < 0:
+        raise ValueError
     pi = 1
     for i in range(1, n_terms + 1):
         pi *= (4 * i ** 2) / (4 * i ** 2 - 1)
