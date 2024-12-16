62daacdfd41396646915135a175868dd8bfa9a94
BenjaminDschps <b.deschamps0723@outlook.fr>
2024-12-16 16:41:38 +0100

    correction indentation

--- numpy_questions.py
+++ numpy_questions.py
@@ -45,7 +45,7 @@ def max_index(X):
     if X.ndim != 2:
         raise ValueError("Input array must be 2-dimensional.")
 
-    i, j = np.unravel_index(np.argmax(X, axis=None), 
+    i, j = np.unravel_index(np.argmax(X, axis=None),
     X.shape)
     return i, j
 
@@ -71,7 +71,7 @@ def wallis_product(n_terms):
     pi_approx = 2.0
     for i in range(1, n_terms + 1):
         pi_approx *= (
-        (2 * i / (2 * i - 1)) * 
-        (2 * i / (2 * i + 1))
+            (2 * i / (2 * i - 1)) *
+            (2 * i / (2 * i + 1))
         )
     return pi_approx
