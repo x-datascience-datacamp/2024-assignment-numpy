212c18abe9b177baf6277034fe184ae31c3ad080
elhcs <elhoussaine.chahboun@gmail.com>
2024-12-16 15:45:17 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -2,18 +2,18 @@
 
 The goals of this assignment are:
     * Use numpy in practice with two easy exercises.
-    * Use automated tools to validate the code (`pytest` and `flake8`)
-    * Submit a Pull-Request on github to practice `git`.
+    * Use automated tools to validate the code (⁠ pytest ⁠ and ⁠ flake8 ⁠)
+    * Submit a Pull-Request on github to practice ⁠ git ⁠.
 
 The two functions below are skeleton functions. The docstrings explain what
 are the inputs, the outputs and the expected error. Fill the function to
 complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
+wrote. To run the tests, use ⁠ pytest test_numpy_question.py ⁠ at the root of
 the repo. It should say that 2 tests ran with success.
 
 We also ask to respect the pep8 convention: https://pep8.org.
-This will be enforced with `flake8`. You can check that there is no flake8
-errors by calling `flake8` at the root of the repo.
+This will be enforced with ⁠ flake8 ⁠. You can check that there is no flake8
+errors by calling ⁠ flake8 ⁠ at the root of the repo.
 """
 import numpy as np
 
@@ -37,10 +37,14 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
 
-    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError("The input is not a numpy array")
+
+    if X.ndim != 2:
+        raise ValueError("The shape is not 2D")
+
+    i,j = np.unravel_index(X.argmax(), X.shape)
 
     return i, j
 
@@ -54,14 +58,31 @@ def wallis_product(n_terms):
     Parameters
     ----------
     n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
+        Number of steps in the Wallis product. Note that ⁠ n_terms=0 ⁠ will
+        consider the product to be ⁠ 1 ⁠.
 
     Returns
     -------
     pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
+        The approximation of order ⁠ n_terms ⁠ of pi using the Wallis product.
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    if n_terms == 0:
+        return 2
+
+    terms = [(2 * n / (2 * n - 1)) * (2 * n / (2 * n + 1)) for n in range(1, n_terms + 1)]
+
+    pi_approximation = 2 * np.prod(terms)
+
+    return pi_approximation
+
+
+
+
+
+
+
+
+
