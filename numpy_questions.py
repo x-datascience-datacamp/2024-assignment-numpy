8d27b88b898fe458d5f4d55cbe58b84e9afde1a3
Jaffar Hussein <jaffhussein@icloud.com>
2024-12-16 18:34:25 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -39,9 +39,11 @@ def max_index(X):
     """
     i = 0
     j = 0
-
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise ValueError()
     # TODO
-
+    max_val = np.max(X)
+    i, j = np.where(X == max_val)
     return i, j
 
 
@@ -64,4 +66,9 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2
+    product = 1
+    for i in range(1, n_terms+1):
+        product *= (4 * i**2) / (4 * i**2 - 1)
+    return 2 * product
