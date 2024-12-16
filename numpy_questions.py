42d056efcc832176e2810da3bcb289ae2efe6e0f
BenjaminDschps <b.deschamps0723@outlook.fr>
2024-12-16 16:30:22 +0100

    correct type code

--- numpy_questions.py
+++ numpy_questions.py
@@ -29,7 +29,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
@@ -37,10 +37,6 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
-
-    # TODO
     # Check if the input is a numpy array
     if not isinstance(X, np.ndarray):
         raise ValueError("Input must be a numpy array.")
@@ -49,7 +45,7 @@ def max_index(X):
     if X.ndim != 2:
         raise ValueError("Input array must be 2-dimensional.")
 
-    i, j=np.unravel_index(np.argmax(X, axis=None), X.shape)
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
     return i, j
 
 
@@ -67,12 +63,11 @@ def wallis_product(n_terms):
 
     Returns
     -------
-    pi : float
+    pi_approx : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    pi = 2.0
-    for i in range(1, n_terms+1):
-        pi*=(2*i/(2*i-1))*(2*i/(2*i+1))
-    return pi
+    # The n_terms is an int that corresponds to the number of terms in the product.
+    pi_approx = 2.0
+    for i in range(1, n_terms + 1):
+        pi_approx *= (2 * i / (2 * i - 1)) * (2 * i / (2 * i + 1))
+    return pi_approx
