02532d0be06b61d62803b2a78291355111f02e79
calls9-amirbraham <amirbrahamm@gmail.com>
2024-12-17 10:05:26 +0100

    code style check

--- numpy_questions.py
+++ numpy_questions.py
@@ -43,9 +43,7 @@ def max_index(X):
         raise ValueError("Input must be a numpy array")
     if X.ndim != 2:
         raise ValueError("Input array must be 2D")
-    
     i, j = np.unravel_index(np.argmax(X), X.shape)
-
     return i, j
 
 
