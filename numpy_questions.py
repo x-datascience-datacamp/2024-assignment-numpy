d0b22f620cc13e3a862f93ffab014161db95ae34
Karim <rochdkarim02@gmail.com>
2024-12-18 09:44:26 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -14,10 +14,12 @@ We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
+
 import numpy as np
 
 
 def max_index(X):
+
     """Return the index of the maximum in a numpy array.
     Parameters
     ----------
@@ -33,6 +35,7 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+
     i = 0
     j = 0
     # TODO
@@ -46,6 +49,7 @@ def max_index(X):
 
 
 def wallis_product(n_terms):
+
     """Implement the Wallis product to compute an approximation of pi.
     See:
     https://en.wikipedia.org/wiki/Wallis_product
@@ -59,6 +63,7 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
+
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
 
