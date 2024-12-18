71c38b6cccf4fdc44966615b61dfeda704eac3b2
Nour <jennanenour@gmail.com>
2024-12-18 20:51:28 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -14,70 +14,70 @@ the repo. It should say that 2 tests ran with success.
 We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
-
-Exercise 1
 """
 import numpy as np
 
 
 def max_index(X):
-   """
-    Return the index of the maximum value in a 2D numpy array.
+    """Return the index of the maximum in a numpy array.
 
     Parameters
     ----------
-    X : np.ndarray
-        A 2D numpy array.
+    X : ndarray of shape (n_samples, n_features)
+        The input array.
 
     Returns
     -------
-    tuple
-        A tuple (i, j) representing the row and column index of the maximum value.
+    (i, j) : tuple(int)
+        The row and columnd index of the maximum.
 
     Raises
     ------
     ValueError
-        If the input is not a 2D numpy array.
+        If the input is not a numpy array or
+        if the shape is not 2D.
     """
-    if not isinstance(X, np.ndarray):
-        raise ValueError("Input has to be a numpy array.")
-    if X.ndim != 2:
-        raise ValueError("Input array must be 2D.")
 
-    max_idx = np.argmax(X)  # Find the maximum value of the array
-    i, j = divmod(max_idx, X.shape[1])  # Returns the indexes of the maximum
+    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input  has ot be a numpy array.")
+    if not isinstance (X.ndim != 2) :
+        raise ValueError("Input array must be 2D.")
+    max_idx = np.argmax(X) #find the maximum value of the array 
+    
+    i, j = divmod(max_idx, X.shape[1])
 
     return i, j
 
 
-"""Exercise 2"""
-
-
 def wallis_product(n_terms):
-   """
-    Compute an approximation of pi using the Wallis product.
+    """Implement the Wallis product to compute an approximation of pi.
+
+    See:
+    https://en.wikipedia.org/wiki/Wallis_product
 
     Parameters
     ----------
     n_terms : int
-        The number of terms to compute in the Wallis product.
+        Number of steps in the Wallis product. Note that `n_terms=0` will
+        consider the product to be `1`.
 
     Returns
     -------
-    float
-        The approximation of pi based on the Wallis product.
-
-    Raises
-    ------
-    Exception
-        If the input is not an integer.
+    pi : float
+        The approximation of order `n_terms` of pi using the Wallis product.
     """
-    if not isinstance(n_terms, int):
-        raise Exception("Input has to be an integer.")
-    if n_terms == 0:
-        return 2.0  # By definition, return 2 for 0 terms
-    product = 1.0
-    for n in range(1, n_terms + 1):
-        product *= (4 * n ** 2) / ((4 * n ** 2) - 1)
-
-    return 2 * product
+    # XXX : The n_terms is an int that corresponds to the number of
+    # terms in the product. For example 10000.
+   if not isinstance(n_terms, int): 
+        raise Exception ('Input has to be a numpy array')
+    if n_terms ==0:
+        return 1 
+    product = 1
+    for n in range(1, n_terms+1):
+        term = (4 * n ** 2) / ((4 * n ** 2) - 1)
+        product *= term
+        
+    pi_approximation = 2* product 
+    return pi_approximation 
+   
