a3bb1d84697198ae7fa9fc40b83eff4dac476652
Nour <jennanenour@gmail.com>
2024-12-18 16:57:07 +0100

    My solutions numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -14,54 +14,40 @@ the repo. It should say that 2 tests ran with success.
 We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
+
+Exercise 1 
 """
+
+
 import numpy as np
 
 
+
 def max_index(X):
-    """Return the index of the maximum in a numpy array.
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input  has ot be a numpy array.")
+    if not isinstance (X.ndim != 2) :
+        raise ValueError("Input array must be 2D.")
+    max_idx = np.argmax(X) #find the maximum value of the array 
+    
+    i, j = divmod(max_idx, X.shape[1])   # returns the indexes fo the maximum 
+    
+    return i,j
 
-    Parameters
-    ----------
-    X : ndarray of shape (n_samples, n_features)
-        The input array.
 
-    Returns
-    -------
-    (i, j) : tuple(int)
-        The row and columnd index of the maximum.
-
-    Raises
-    ------
-    ValueError
-        If the input is not a numpy array or
-        if the shape is not 2D.
-    """
-    i = 0
-    j = 0
-
-    # TODO
-
-    return i, j
-
+"""Exercise 2"""
 
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
+    if not isinstance(n_terms, int): 
+        raise Exception ('Input has to be a numpy array')
+    if n_terms ==0:
+        return 1 
+    product = 1 
+    for n in range(1, n_terms+1):
+        term = (4 * n ** 2) / ((4 * n ** 2) - 1)
+        product *= term
+        
+    pi_approximation = 2* product 
+    return pi_approximation 
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
 
-    Parameters
-    ----------
-    n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
-
-    Returns
-    -------
-    pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
-    """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
